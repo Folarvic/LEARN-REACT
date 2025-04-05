0a8e8ca9790d1636fb39c16a9273e74e623c8f83
Samson <Samfolarivc@gmail.com>
2025-04-05 18:24:45 +0100

    p3 ready

--- learning-app/src/App.jsx
+++ learning-app/src/App.jsx
@@ -1,24 +1,11 @@
-import Header from "./components/Header"
-import Entry from "./components/Entry"
-import data from "./data"
+import Header from "./Header"
+import Main from "./Main"
 
 export default function App() {
-    
-    const entryElements = data.map((entry) => {
-        return (
-            <Entry
-                key={entry.id}
-                entry={entry}
-            />
-        )
-    })
-    
-    return (
-        <>
-            <Header />
-            <main className="container">
-                {entryElements}
-            </main>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
 }
